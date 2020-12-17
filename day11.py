c3ba42e426e0852cb6a718d2f3d914cd117638a0
Sam Jones <sam.jones@splend.com>
2020-12-17 20:19:45 +0000

    day 11 part 1

--- day11.py
+++ day11.py
@@ -1,6 +1,6 @@
 import advent_helpers
 from typing import List
-
+import copy
 
 def get_adjacent_ords(row, col):
 	return {(row-1, col - 1), (row - 1, col), (row - 1, col + 1), (row, col - 1), 
@@ -17,23 +17,45 @@ def list_to_str(arr: List[List[str]]):
 	return output
 
 
-def part_1(problem_input: List[str]):
-	for i, row in enumerate(problem_input):
-		for j, seat in enumerate(row):
-			if seat == ".":
-				continue
-			else:
-				adjacent_seats = 0
-				for surrounding in get_adjacent_ords(i, j):
-					if (0 <= surrounding[0] < len(problem_input)) and (0 <= surrounding[1] < len(row)):
-						if problem_input[surrounding[0]][surrounding[1]] == "#":
-							adjacent_seats += 1
-				if adjacent_seats >= 4:
-					problem_input[i][j] = "L"
-				elif adjacent_seats == 0:
-					problem_input[i][j] = "#"
+def count_seats(seat_grid: List[List[str]]):
+	total = 0
+	for row in seat_grid:
+		for j in range(len(row)):
+			if row[j] == "#":
+				total += 1
 
-	print(list_to_str(problem_input))
+	return total
+
+
+def part_1(problem_input: List[List[str]]):
+	max_iterations = 500
+	current = 0
+	while current < max_iterations:
+		current += 1
+		next_state = copy.deepcopy(problem_input)
+		for i, row in enumerate(problem_input):
+			for j, seat in enumerate(row):
+				if seat == ".":
+					continue
+				else:
+					adjacent_seats = 0
+					for surrounding in get_adjacent_ords(i, j):
+						if (0 <= surrounding[0] < len(problem_input)) and (0 <= surrounding[1] < len(row)):
+							if problem_input[surrounding[0]][surrounding[1]] == "#":
+								adjacent_seats += 1
+					if adjacent_seats >= 4:
+						next_state[i][j] = "L"
+					elif adjacent_seats == 0:
+						next_state[i][j] = "#"
+					else:
+						next_state[i][j] = problem_input[i][j]
+
+		if problem_input == next_state:
+			print(f"stabilised after {current} iterations")
+			break
+		problem_input = copy.deepcopy(next_state)
+
+	return count_seats(next_state)
 
 
 
@@ -42,18 +64,18 @@ def part_2(problem_input: List[str]):
 
 
 def main(problem_input):
-	print(part_1(list(map(list, """L.LL.LL.LL
-LLLLLLL.LL
-L.L.L..L..
-LLLL.LL.LL
-L.LL.LL.LL
-L.LLLLL.LL
-..L.L.....
-LLLLLLLLLL
-L.LLLLLL.L
-L.LLLLL.LL""".split("\n")))))
+# 	print(part_1(list(map(list, """L.LL.LL.LL
+# LLLLLLL.LL
+# L.L.L..L..
+# LLLL.LL.LL
+# L.LL.LL.LL
+# L.LLLLL.LL
+# ..L.L.....
+# LLLLLLLLLL
+# L.LLLLLL.L
+# L.LLLLL.LL""".split("\n")))))
 
-	# print(part_1(list(map(int, problem_input.split("\n")[:-1]))))
+	print(part_1(list(map(list, problem_input.split("\n")[:-1]))))
 
 	# print(part_2(list(map(int, problem_input.split("\n")[:-1]))))
 
