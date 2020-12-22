0e8be20b3d7459eec3ce5f91cd7ff9c89889163a
Sam Jones <sam.jones@splend.com>
2020-12-22 14:57:30 +0000

    day12 part 1 done

--- day12.py
+++ day12.py
@@ -3,40 +3,47 @@ from typing import List, Tuple
 
 
 def taxi_distance(point_1: Tuple[int, int], point_2: Tuple[int, int]) -> int:
+	print(point_1, point_2)
 	return abs(point_1[0] - point_2[0]) + abs(point_1[1] - point_2[1])
 
 
 def part_1(problem_input: List[str]):
-	"""950 is too high"""
 	heading = 90 # 0 is north, work clockwise
 	location = (0, 0)
 	for inst in problem_input:
 		direction = inst[0]
 		number = int(inst[1:])
-		if direction == "L" or direction == "R":
+		if direction == "R":
 			if not number % 90 == 0:
 				print("complex turn detected")
 			heading += number
 			heading = heading % 360
+		elif direction == "L":
+			if not number % 90 == 0:
+				print("complex turn detected")
+			heading -= number
+			heading = heading % 360
 		elif direction == "N":
-			location = location[0] + number, location[1]
-		elif direction == "S":
-			location = location[0] - number, location[1]
-		elif direction == "E":
 			location = location[0], location[1] + number
-		elif direction == "W":
+		elif direction == "S":
 			location = location[0], location[1] - number
+		elif direction == "E":
+			location = location[0] + number, location[1]
+		elif direction == "W":
+			location = location[0] - number, location[1]
 		elif direction == "F":
 			if heading == 0:
-				location = location[0] + number, location[1]
-			elif heading == 90:
 				location = location[0], location[1] + number
+			elif heading == 90:
+				location = location[0] + number, location[1]
 			elif heading == 180:
-				location = location[0] - number, location[1]
-			elif heading == 270:
 				location = location[0], location[1] - number
+			elif heading == 270:
+				location = location[0] - number, location[1]
 			else:
 				print("bad heading")
+		else:
+			print("bad instruction")
 
 	return taxi_distance(location, (0, 0))
 
